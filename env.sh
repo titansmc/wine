adb65063960ccbaa27ea7121c11309ffde6782ca
jandres - moscardo <titansmc@gmail.com>
2025-04-02 16:04:15 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 sudo apt install python3.10-venv python3-pip
 python3 -m venv datascience
 source datascience/bin/activate
-pip install pandas matplotlib PyQt6
+pip install pandas matplotlib PyQt6 ucimlrepo
 python3 wine.py
