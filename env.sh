943224a406f51ab9151ab051d492787090710633
Josep Manel Andres Moscardo <josep.moscardo@embl.de>
2025-04-03 10:05:15 +0200

    docu

--- env.sh
+++ env.sh
@@ -1,5 +1,10 @@
+# Instalar paquetes python
 sudo apt install python3.10-venv python3-pip
+# Crear virtual environment en python
 python3 -m venv datascience
+# Activar entorno
 source datascience/bin/activate
+# Instalamos librerias que usa nuestro script
 pip install pandas matplotlib PyQt6 ucimlrepo
+# Ejecutamos el script
 python3 wine.py
